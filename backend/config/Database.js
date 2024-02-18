cde5241bd50125a981bb9e721778ef0d91362183
daffakhairyal <117626673+daffakhairyal@users.noreply.github.com>
2024-02-19 06:46:56 +0700

    test project di laptop baru

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,8 @@
-import {Sequelize} from "sequelize";
+import Sequelize from "sequelize";
 
-const db = new Sequelize('dashboard','root','',{
-    host:"localhost",
-    dialect:"mysql"
+const db = new Sequelize('dashboard', 'user', 'Dd@14170077', {
+    host: "192.168.1.9",
+    dialect: "mysql",
 });
 
-export default db
+export default db;
